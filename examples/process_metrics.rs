831fa0a8f3af24b3c617f71721ec0fb21e898076
Takeru Ohta <phjgt308@gmail.com>
2017-10-04 22:41:59 +0900

    Add example command

---
+++ examples/process_metrics.rs
@@ -0,0 +1,19 @@
+extern crate prometrics;
+#[macro_use]
+extern crate trackable;
+
+use prometrics::metrics::ProcessMetricsCollector;
+use trackable::error::Failure;
+
+fn main() {
+    track_try_unwrap!(prometrics::default_registry().register(
+        ProcessMetricsCollector::new(),
+    ));
+
+    let mut gatherer = track_try_unwrap!(prometrics::default_gatherer().lock().map_err(|e| {
+        Failure::from_error(e.to_string())
+    }));
+    for metric in gatherer.gather() {
+        println!("{}", metric);
+    }
+}
